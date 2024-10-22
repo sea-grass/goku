85dfa2072971a0cbe8287423924be35d21f96816
sea-grass <dev.seagrass@gmail.com>
2024-10-22 11:44:11 -0400

    Move http listener to Server container

--- src/serve.zig
+++ src/serve.zig
@@ -8,7 +8,7 @@ const fmt = std.fmt;
 const heap = std.heap;
 const debug = std.debug;
 
-const log_sitemap = false;
+const log_sitemap = true;
 
 // This is the shape of the program architecture that I envision.
 const shape = .{
@@ -47,7 +47,7 @@ pub fn main() !void {
     }
 
     var server: Server = undefined;
-    server.init(.{
+    try server.init(.{
         .port = 3000,
         .allocator = gpa.allocator(),
         .public_folder = public_folder,
@@ -63,10 +63,8 @@ fn parsePublicFolder(buf: []u8) ![]const u8 {
     // Skip the executable name
     _ = args.next();
 
-    const public_folder = args.next();
-
-    if (public_folder) |p| {
-        return fmt.bufPrint(buf, "{s}", .{p});
+    if (args.next()) |public_folder| {
+        return fmt.bufPrint(buf, "{s}", .{public_folder});
     }
 
     return error.MissingArgs;
@@ -82,6 +80,8 @@ const Server = struct {
         allocator: mem.Allocator,
     };
     pub fn init(self: *Server, options: Options) void {
+        log.info("public: {s}", .{options.public_folder});
+        zap.enableDebugLog();
         self.* = .{
             .allocator = options.allocator,
             .listener = zap.HttpListener.init(.{
@@ -94,8 +94,8 @@ const Server = struct {
                 .public_folder = options.public_folder,
             }),
         };
-        zap.enableDebugLog();
     }
+
     pub fn deinit(self: Server) void {
         _ = self;
     }
@@ -110,6 +110,64 @@ const Server = struct {
             .workers = 1,
         });
     }
+
+    fn onRequest(r: zap.Request) void {
+        handle(r) catch |err| switch (err) {
+            error.NotFound => {
+                r.setStatus(.not_found);
+                r.sendBody("Not found") catch {};
+            },
+            else => {},
+        };
+    }
+
+    fn onUpgrade(r: zap.Request, target_protocol: []const u8) void {
+        if (!mem.eql(u8, target_protocol, "websocket")) {
+            log.warn("Received illegal protocol: {s}", .{target_protocol});
+            r.setStatus(.bad_request);
+            r.sendBody("400 Bad Request") catch @panic("Failed to send 400 response");
+            return;
+        }
+
+        // I don't want to use a global variable, but they have it in their example
+        // I think I'll need to make this an endpoint.
+    }
+
+    fn handle(r: zap.Request) !void {
+        // TODO websocket feature check will be via a runtime arg
+        if (comptime enable_websocket and mem.eql(u8, r.path.?, "/ws")) {
+            try handleWebsocket(r);
+        } else {
+            try handleFallback(r);
+        }
+    }
+
+    fn handleWebsocket(r: zap.Request) !void {
+        _ = r;
+    }
+
+    // Zap will fall back to this handler if it does not find a matching file in the public folder
+    // This handler will try to redirect requests for directories to their index.html file, if possible.
+    fn handleFallback(r: zap.Request) error{ NotFound, Foobie, MalformedRequestPath }!void {
+        const path = r.path.?;
+
+        var it = mem.splitBackwardsScalar(u8, path, '/');
+        const last_part = it.next() orelse return error.MalformedRequestPath;
+
+        if (mem.lastIndexOfScalar(u8, last_part, '.') == null) {
+            // honestly, this isn't even the best approach...what if the url just has a `.` in it?
+            // TODO probably a better constant to use for max url length
+            var buf: [fs.max_path_bytes]u8 = undefined;
+            const new_path = fmt.bufPrint(&buf, "{s}{s}{s}", .{
+                path,
+                if (mem.endsWith(u8, path, "/")) "" else "/",
+                "index.html",
+            }) catch return error.Foobie;
+            r.redirectTo(new_path, .temporary_redirect) catch return error.Foobie;
+        }
+
+        return error.NotFound;
+    }
 };
 
 // Assumes that the `public_folder_path` points to a Goku site with a generated sitemap.html.
@@ -122,58 +180,4 @@ fn readSitemap(allocator: mem.Allocator, public_folder_path: []const u8) ![]cons
     defer file.close();
 
     return try file.readToEndAlloc(allocator, 1 * 1024 * 1024);
-}
-
-fn onRequest(r: zap.Request) void {
-    handle(r) catch |err| switch (err) {
-        error.NotFound => {
-            r.setStatus(.not_found);
-            r.sendBody("Not found") catch {};
-        },
-        else => {},
-    };
-}
-
-fn onUpgrade(r: zap.Request, target_protocol: []const u8) void {
-    if (!mem.eql(u8, target_protocol, "websocket")) {
-        log.warn("Received illegal protocol: {s}", .{target_protocol});
-        r.setStatus(.bad_request);
-        r.sendBody("400 Bad Request") catch @panic("Failed to send 400 response");
-        return;
-    }
-
-    // I don't want to use a global variable, but they have it in their example
-    // I think I'll need to make this an endpoint.
-}
-
-fn handle(r: zap.Request) !void {
-    // TODO websocket feature check will be via a runtime arg
-    if (comptime enable_websocket and mem.eql(u8, r.path.?, "/ws")) {
-        try handleWebsocket(r);
-    } else {
-        try handleFallback(r);
-    }
-}
-
-fn handleWebsocket(r: zap.Request) !void {
-    _ = r;
-}
-
-// Zap will fall back to this handler if it does not find a matching file in the public folder
-// This handler will try to redirect requests for directories to their index.html file, if possible.
-fn handleFallback(r: zap.Request) error{ NotFound, Foobie, MalformedRequestPath }!void {
-    const path = r.path.?;
-
-    var it = mem.splitBackwardsScalar(u8, path, '/');
-    const last_part = it.next() orelse return error.MalformedRequestPath;
-
-    if (mem.lastIndexOfScalar(u8, last_part, '.') == null) {
-        // honestly, this isn't even the best approach...what if the url just has a `.` in it?
-        // TODO probably a better constant to use for max url length
-        var buf: [fs.max_path_bytes]u8 = undefined;
-        const new_path = fmt.bufPrint(&buf, "{s}/{s}", .{ path, "index.html" }) catch return error.Foobie;
-        r.redirectTo(new_path, .temporary_redirect) catch return error.Foobie;
-    }
-
-    return error.NotFound;
 }
