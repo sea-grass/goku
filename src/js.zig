4a702963057884ad0fe694231b4e02cd2fbad6e4
sea-grass <dev.seagrass@gmail.com>
2024-10-29 19:36:29 -0400

    Update JS test to count number of times a defined c function is called.

--- src/js.zig
+++ src/js.zig
@@ -6,54 +6,6 @@ const mem = std.mem;
 const std = @import("std");
 const testing = std.testing;
 
-const GokuMod = struct {
-    fn define(ctx: *c.JSContext) !*c.JSModuleDef {
-        const mod = c.JS_NewCModule(ctx, "goku", init) orelse return error.CannotInitGokuModule;
-        addExports(ctx, mod);
-        return mod;
-    }
-
-    fn init(ctx: ?*c.JSContext, m: ?*c.JSModuleDef) callconv(.C) c_int {
-        _ = c.JS_SetModuleExport(
-            ctx,
-            m,
-            "print",
-            c.JS_NewCFunction(
-                ctx,
-                print,
-                "print",
-                1,
-            ),
-        );
-
-        return 0;
-    }
-
-    fn print(ctx: ?*c.JSContext, this: c.JSValueConst, argc: c_int, argv: ?*c.JSValueConst) callconv(.C) c.JSValue {
-        _ = this;
-        debug.assert(argc == 1);
-
-        const ptr_maybe = argv.?[0..1][0];
-
-        const str = c.JS_ToCString(ctx, ptr_maybe);
-        defer c.JS_FreeCString(ctx, str);
-
-        const stderr = io.getStdErr().writer();
-        stderr.print("{s}\n", .{str}) catch unreachable;
-
-        return .{
-            .tag = c.JS_TAG_UNDEFINED,
-            .u = .{
-                .int32 = 0,
-            },
-        };
-    }
-
-    fn addExports(ctx: *c.JSContext, mod: *c.JSModuleDef) void {
-        _ = c.JS_AddModuleExport(ctx, mod, "print");
-    }
-};
-
 // MallocFunctions should generate a struct with allocation functions
 // usable by a QuickJS runtime, backed by a Zig allocator.
 // c.JSMallocState.opaque holds a pointer that we can use to access the allocator.
@@ -91,7 +43,7 @@ const MallocFunctions = struct {
         self.map.deinit();
     }
 
-    fn js_malloc_functions(self: *MallocFunctions) [*c]c.JSMallocFunctions {
+    pub fn js_malloc_functions(self: *MallocFunctions) [*c]c.JSMallocFunctions {
         _ = self;
 
         return @constCast(@ptrCast(&MallocFunctions._js_malloc_functions));
@@ -99,55 +51,24 @@ const MallocFunctions = struct {
 
     fn malloc(state: [*c]c.JSMallocState, n: usize) callconv(.C) ?*anyopaque {
         const self = fromState(state);
-
-        // TODO I can't errdefer free here because this fn doesn't return an error.
-        // I need to refactor these malloc implementations to call an internal,
-        // error union-returning variant in order to express memory safety better.
-        const slice = self.allocator.alloc(u8, n) catch return null;
-        errdefer self.allocator.free(slice);
-
-        const ptr: ?*anyopaque = @ptrCast(slice);
-
-        self.map.put(ptr, .{ .slice = slice }) catch {
-            self.allocator.free(slice);
-            return null;
-        };
-
-        return ptr;
+        return mallocInner(self, n) catch null;
     }
 
     fn free(state: [*c]c.JSMallocState, ptr: ?*anyopaque) callconv(.C) void {
-        if (ptr == null) {
-            return;
-        }
-
+        if (ptr == null) return;
         const self = fromState(state);
-        const entry = self.map.fetchRemove(ptr) orelse {
-            @panic("Tried to free unowned memory.");
+
+        return freeInner(self, ptr) catch {
+            @panic("Could not free memory");
         };
-        const alloc_unit = entry.value;
-        self.allocator.free(alloc_unit.slice);
     }
 
     fn realloc(state: [*c]c.JSMallocState, ptr: ?*anyopaque, n: usize) callconv(.C) ?*anyopaque {
         if (ptr == null) return malloc(state, n);
 
         const self = fromState(state);
-        // TODO better error handling
-        const entry = self.map.fetchRemove(ptr.?) orelse return null;
-        // TODO better error handling
-        const slice = self.allocator.realloc(
-            entry.value.slice,
-            n,
-        ) catch return null;
-        const new_ptr: ?*anyopaque = @ptrCast(slice);
 
-        self.map.put(new_ptr, .{ .slice = slice }) catch {
-            self.allocator.free(slice);
-            return null;
-        };
-
-        return new_ptr;
+        return reallocInner(self, ptr, n) catch null;
     }
 
     // The man pages for `malloc_usable_size` declare that this function
@@ -161,40 +82,47 @@ const MallocFunctions = struct {
         return 0;
     }
 
+    fn mallocInner(self: *MallocFunctions, n: usize) !?*anyopaque {
+        const slice = try self.allocator.alloc(u8, n);
+        errdefer self.allocator.free(slice);
+
+        const ptr: ?*anyopaque = @ptrCast(slice);
+
+        try self.map.put(ptr, .{ .slice = slice });
+
+        return ptr;
+    }
+
+    fn freeInner(self: *MallocFunctions, ptr: ?*anyopaque) !void {
+        const entry = self.map.fetchRemove(ptr) orelse {
+            return error.TriedToFreeUnownedMemory;
+        };
+
+        self.allocator.free(entry.value.slice);
+    }
+
+    fn reallocInner(self: *MallocFunctions, ptr: ?*anyopaque, n: usize) !?*anyopaque {
+        // TODO should I fetch and only remove after success?
+        const entry = self.map.fetchRemove(ptr.?) orelse {
+            return error.TriedToReallocUnknownMemory;
+        };
+
+        const slice = self.allocator.realloc(entry.value.slice, n) catch {
+            return null;
+        };
+        errdefer self.allocator.free(slice);
+
+        const new_ptr: ?*anyopaque = @ptrCast(slice);
+
+        try self.map.put(new_ptr, .{ .slice = slice });
+
+        return new_ptr;
+    }
+
     fn fromState(state: [*c]c.JSMallocState) *MallocFunctions {
         return @alignCast(@ptrCast(state.*.@"opaque".?));
     }
 };
-
-fn js_goku_init(ctx: ?*c.JSContext, m: ?*c.JSModuleDef) callconv(.C) c_int {
-    _ = c.JS_SetModuleExport(
-        ctx,
-        m,
-        "exit",
-        c.JS_NewCFunction(ctx, js_exit, "exit", 1),
-    );
-
-    return 0;
-}
-
-fn js_exit(ctx: ?*c.JSContext, this: c.JSValueConst, argc: c_int, argv: ?*c.JSValueConst) callconv(.C) c.JSValue {
-    _ = this;
-    _ = argc;
-
-    var status: c_int = undefined;
-    if (c.JS_ToInt32(ctx, &status, argv.?[0..1][0]) == 1) {
-        @panic("Encountered an error trying to parse args.");
-    }
-
-    io.getStdErr().writer().print("Exit code: {d}\n", .{status}) catch @panic("Unexpected error");
-
-    return .{
-        .tag = c.JS_TAG_UNDEFINED,
-        .u = .{
-            .int32 = 0,
-        },
-    };
-}
 
 test "console not defined" {
     const rt = c.JS_NewRuntime();
@@ -222,6 +150,7 @@ test "console not defined" {
 }
 
 test "provide print" {
+    // Check for memory leaks with custom malloc using testing.allocator
     var custom_malloc = MallocFunctions.init(testing.allocator);
     defer custom_malloc.deinit();
 
@@ -234,15 +163,57 @@ test "provide print" {
     const ctx = c.JS_NewContext(rt) orelse return error.CannotAllocateJSContext;
     defer c.JS_FreeContext(ctx);
 
-    _ = try GokuMod.define(ctx);
+    // Add userdata to context to be able to assert our c function is called.
+    var times_called: u8 = 0;
+    c.JS_SetContextOpaque(ctx, @ptrCast(&times_called));
+
+    // Define the module that will provide the import.
+    {
+        const mod_source = struct {
+            fn init(_ctx: ?*c.JSContext, m: ?*c.JSModuleDef) callconv(.C) c_int {
+                const func = c.JS_NewCFunction(_ctx, print, "print", 1);
+                _ = c.JS_SetModuleExport(_ctx, m, "print", func);
+                return 0;
+            }
+
+            fn print(_ctx: ?*c.JSContext, this: c.JSValueConst, argc: c_int, argv: ?*c.JSValueConst) callconv(.C) c.JSValue {
+                _ = this;
+                debug.assert(argc == 1);
+
+                const str = c.JS_ToCString(_ctx, argv.?[0..1][0]);
+                defer c.JS_FreeCString(_ctx, str);
+
+                const slice = mem.span(str);
+                debug.assert(slice.len == "Hello, world".len);
+
+                const n: *u8 = @ptrCast(c.JS_GetContextOpaque(_ctx));
+                n.* += 1;
+
+                return .{
+                    .tag = c.JS_TAG_UNDEFINED,
+                    .u = .{
+                        .int32 = 0,
+                    },
+                };
+            }
+        };
+
+        const mod = c.JS_NewCModule(ctx, "mod", mod_source.init);
+        _ = c.JS_AddModuleExport(ctx, mod, "print");
+    }
 
     const prog =
-        \\import * as goku from 'goku';
-        \\goku.print("Hello, world");
+        \\import { print } from "mod";
+        \\var str = "Some str";
+        \\str = "Hello, ";
+        \\str += "world";
+        \\print(str);
     ;
 
     const val = c.JS_Eval(ctx, prog, prog.len, "<main>", c.JS_EVAL_TYPE_MODULE);
     defer c.JS_FreeValue(ctx, val);
+
+    try testing.expectEqual(1, times_called);
 }
 
 fn expectException(comptime expected: []const u8, ctx: *c.JSContext) !void {
