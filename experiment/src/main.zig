342959befabf453be2908d1c24e2747166c3f95d
sea-grass <dev.seagrass@gmail.com>
2025-04-16 13:37:29 -0400

    Add site fs validation

--- experiment/src/main.zig
+++ experiment/src/main.zig
@@ -17,10 +17,44 @@ pub fn main() !void {
     };
     defer allocator.free(site_path);
 
-    debug.assert(site_path.len > 0);
+    debug.assert(std.fs.path.isAbsolute(site_path));
 
     log.info("site{s}", .{site_path});
+
+    var site_dir = try std.fs.openDirAbsolute(site_path, .{});
+    defer site_dir.close();
+
+    try validateSiteDir(&site_dir);
 }
+
+fn validateSiteDir(dir: *fs.Dir) !void {
+    if (dir.statFile("pages")) |stat| switch (stat.kind) {
+        .directory => {},
+        else => return error.PagesIsNotADir,
+    } else |err| switch (err) {
+        error.FileNotFound => return error.MissingPagesDir,
+        else => return err,
+    }
+
+    if (dir.statFile("templates")) |stat| switch (stat.kind) {
+        .directory => {},
+        else => return error.TemplatesIsNotADir,
+    } else |err| switch (err) {
+        error.FileNotFound => return error.MissingTemplatesDir,
+        else => return err,
+    }
+
+    if (dir.statFile("components")) |stat| switch (stat.kind) {
+        .directory => {},
+        else => return error.PagesIsNotADir,
+    } else |err| switch (err) {
+        // components dir is optional
+        error.FileNotFound => {},
+        else => return err,
+    }
+}
+
 const debug = std.debug;
+const fs = std.fs;
 const log = std.log.scoped(.experiment);
 const std = @import("std");
